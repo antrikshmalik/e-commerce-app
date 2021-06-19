319ddb2d0345b89784b50eda34047eefcaff1fbd
antrikshmalik <malikantriksh@gmail.com>
2021-06-19 13:32:09 +0530

    checkout-item component completed

--- src/page/checkout/checkout.jsx
+++ src/page/checkout/checkout.jsx
@@ -3,6 +3,7 @@ import "./checkout.scss";
 import {connect} from "react-redux";
 import {createStructuredSelector} from "reselect";
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cart-selector";
+import CheckoutItem from "../../components/checkout-item/checkout-item";
 
 const Checkout = ({cartItems, total}) => {
     return (
@@ -26,7 +27,7 @@ const Checkout = ({cartItems, total}) => {
             </div>
             {
                 cartItems.map(cartItem=>
-                    cartItem.name
+                   <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
                 )
             }
             <div className="total">
